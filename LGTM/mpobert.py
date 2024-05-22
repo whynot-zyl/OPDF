c965a04b1176e97eca7362218e300b5766bd0e3b
詹玉梁 <58316122+whynot-zyl@users.noreply.github.com>
2024-05-22 23:07:13 +0800

    Add files via upload

---
+++ LGTM/mpobert.py
@@ -0,0 +1,34 @@
+from transformers import BertModel, BertPreTrainedModel, BertConfig
+import torch.nn as nn
+
+class BertModelCustom(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.bert = BertModel(config)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=False,
+            output_hidden_states=True
+        )
+
+        pooled_output = outputs[1]
+
+        logits = self.classifier(pooled_output)
+
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            return loss, logits, outputs.hidden_states
+
+        return logits, outputs.hidden_states
+
